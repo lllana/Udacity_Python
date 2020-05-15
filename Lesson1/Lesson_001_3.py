b6baea7ba32e9e304adecb94894947bafd06843b
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-15 16:29:22 +0100

    Create Lesson_001_3.py

---
+++ Lesson1/Lesson_001_3.py
@@ -0,0 +1,11 @@
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+weakly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+weakly_sales = str(weakly_sales)
+messsge = "This week's total sales: " + weakly_sales
+print(messsge)
