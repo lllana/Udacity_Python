f023ec02a1a5c56f22654bc304d3eac660fddce5
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-19 17:40:03 +0100

    Update Lesson_003_10.py

--- Lesson3/Lesson_003_10.py
+++ Lesson3/Lesson_003_10.py
@@ -10,3 +10,13 @@ for key in cast:
 
 for key, value in cast.items():
     print("Actor: {}    Role: {}".format(key, value))
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object, count in basket_items.items():
+   if object in fruits:
+       result += count
+
+print("There are {} fruits in the basket.".format(result))
