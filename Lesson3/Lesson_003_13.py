2f287976209c78f19d2ef30d28f4b9f2574ba6af
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-20 18:23:18 +0100

    Update Lesson_003_13.py

--- Lesson3/Lesson_003_13.py
+++ Lesson3/Lesson_003_13.py
@@ -16,3 +16,17 @@ while current <= number:
 
 # print the factorial of number
 print(product)
+
+
+# number to find the factorial of
+number = 6
+
+# start with our product equal to one
+product = 1
+
+# write your for loop here
+for i in range(2, number + 1):
+    product *= i
+
+# print the factorial of number
+print(product)
