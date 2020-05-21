12ed8063f4f8b2064cc626bd4b0645854c854315
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-21 16:47:41 +0100

    Update Lesson_003_20.py

--- Lesson3/Lesson_003_20.py
+++ Lesson3/Lesson_003_20.py
@@ -10,3 +10,39 @@ for point in zip(labels, x_coord, y_coord, z_coord):
 
 for point in points:
     print(point)
+
+
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names,cast_heights))# replace with your code
+print(cast)
+
+#unzip to tuples
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names, heights = zip(*cast)
+
+print(names)
+print(heights)
+
+#Quiz: Transpose with Zip
+
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+a,b,c = zip(*data)
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+
+#Enumerate
+
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for i, character in enumerate(cast):
+    cast[i] = character + " " + str(heights[i])
+
+print(cast)
