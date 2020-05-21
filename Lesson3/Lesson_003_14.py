fff2f7e04287c9113c54fbe6c02fcb70218fe6d8
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-21 10:48:26 +0100

    Create Lesson_003_14.py

---
+++ Lesson3/Lesson_003_14.py
@@ -0,0 +1,11 @@
+start_num = 0 #provide some start number
+end_num = 90 #provide some end number that you stop when you hit
+count_by = 10 #provide some number to count by
+
+# write a while loop that uses break_num as the ongoing number to
+break_num = start_num
+while break_num < end_num:
+    break_num += count_by
+#   check against end_num
+
+print(break_num)
