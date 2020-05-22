0cc9b5881d504205cc080127345406b7b5021fc2
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-22 17:36:23 +0100

    Create Lesson_004_2.py

---
+++ Lesson4/Lesson_004_2.py
@@ -0,0 +1,13 @@
+# egg_count = 0
+#
+# def buy_eggs():
+#     egg_count += 12 # purchase a dozen eggs
+#
+# buy_eggs()
+
+egg_count = 0
+
+def buy_eggs(count):
+    return count + 12  # purchase a dozen eggs
+
+egg_count = buy_eggs(egg_count)
