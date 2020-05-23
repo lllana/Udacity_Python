e6faf1e4e8f415125eb25cd9fea1e9365343be8b
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-23 19:24:40 +0100

    add files

--- Lesson1/Lesson_001_1.py
+++ Lesson1/Lesson_001_1.py
@@ -4,7 +4,7 @@ url = "http://petshop.com/pets/mammals/cats"
 # TODO: print a log message using the variables above.
 # The message should have the same format as this one:
 # "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
-print (username + " accessed the site " + url + " at " + timestamp + ".")
+print(username + " accessed the site " + url + " at " + timestamp + ".")
 
 message = username + " accessed the site " + url + " at " + timestamp + "."
 print(message)
