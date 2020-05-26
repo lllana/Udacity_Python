7724a0da04339fb22b530f9f2f8de14f0c216745
Lana Rubchenia <svetarubchenya@gmail.com>
2020-05-26 16:17:39 +0100

    add files

---
+++ Lesson6_NumPy/Lesson_06_8.py
@@ -0,0 +1,26 @@
+import numpy as np
+# We create two rank 1 ndarrays
+x = np.array([1,2,3,4])
+y = np.array([5.5,6.5,7.5,8.5])
+
+# We print x
+print()
+print('x = ', x)
+
+# We print y
+print()
+print('y = ', y)
+print()
+
+# We perfrom basic element-wise operations using arithmetic symbols and functions
+print('x + y = ', x + y)
+print('add(x,y) = ', np.add(x,y))
+print()
+print('x - y = ', x - y)
+print('subtract(x,y) = ', np.subtract(x,y))
+print()
+print('x * y = ', x * y)
+print('multiply(x,y) = ', np.multiply(x,y))
+print()
+print('x / y = ', x / y)
+print('divide(x,y) = ', np.divide(x,y))
